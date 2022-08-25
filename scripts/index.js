feaee683451854fba45f10deda78efe59c4eaf03
CoddingMike <maxi_faggiani@hotmail.com>
2022-08-25 09:02:22 -0300

    Avance Proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,59 @@
+import { productSingleMaltWhisky } from "./products.js";
+import { productBlendedWhisky } from "./products.js";
+import { productBourbonWhisky } from "./products.js";
 import { productIrishWhisky } from "./products.js";
+
 const scotchWhisky = document.getElementById("scotch-whisky");
+const blendedWhisky = document.getElementById("blended-whisky");
 const bourbonWhisky = document.getElementById("bourbon-whisky");
 const irishWhisky = document.getElementById("irish-whisky");
-const ryeWhisky = document.getElementById("rye-whisky");
 const productSelection = document.getElementsByClassName("product-detail");
-const titleItem = "<h4 class='item-title'</h4>";
+const titleSelection = document.getElementsByClassName("item-title");
+const descriptionSelect = document.getElementsByClassName("description-item");
+const priceItemSelect = document.getElementsByClassName("price-item");
+const titleItem = "<h4 class='item-title'></h4>";
+const descriptionItem = "<p class='description-item'></p>";
+const priceItem = "<p class='price-item'></p>";
 const classImage = "class='image-product-style'>";
 const buttonItem = "<button class='button-selection'><i class='fa-solid fa-cart-plus'></i></button>";
 
 const scotishSelection = () => {
-
 for (let i = 0; i < 4; i++) {
-        productSelection[i].innerHTML = titleItem + "<img src='/img/NODISP.JPG'" + classImage + buttonItem;
+        productSelection[i].innerHTML = titleItem + descriptionItem + priceItem + "<img src='/img/NODISP.JPG'" + classImage + buttonItem;
+        titleSelection[i].innerText = productSingleMaltWhisky[i].name;
+        descriptionSelect[i].innerText = "Tamaño:" + productSingleMaltWhisky[i].size;
+        priceItemSelect[i].innerText = "Precio: " + productSingleMaltWhisky[i].price;
+    }  
+}
+
+const blendedSelection = () => {
+    for (let i = 0; i < 4; i++) {
+        productSelection[i].innerHTML = titleItem + descriptionItem + priceItem + "<img src='/img/NODISP.JPG'" + classImage + buttonItem;
+        titleSelection[i].innerText = productBlendedWhisky[i].name;
+        descriptionSelect[i].innerText = "Tamaño:" + productBlendedWhisky[i].size;
+        priceItemSelect[i].innerText = "Precio: " + productBlendedWhisky[i].price;
     }  
 }
 
 const buorbonSelection = () => {
     for (let i = 0; i < 4; i++) {
-        productSelection[i].innerHTML = "<h4></h4><img src='/img/NODISP.JPG'" + classImage + buttonItem;
-    }
+        productSelection[i].innerHTML = titleItem + descriptionItem + priceItem + "<img src='/img/NODISP.JPG'" + classImage + buttonItem;
+        titleSelection[i].innerText = productBourbonWhisky[i].name;
+        descriptionSelect[i].innerText = "Tamaño:" + productBourbonWhisky[i].size;
+        priceItemSelect[i].innerText = "Precio: " + productBourbonWhisky[i].price;
+    }  
 }
 
 const irishSelection = () => {
     for (let i = 0; i < 4; i++) {
-        productSelection[i].innerHTML = "<h4></h4><img src='/img/NODISP.JPG'" + classImage + buttonItem;
-    }
-}
-
-const ryeSelection = () => {
-    for (let i = 0; i < 4; i++) {
-        productSelection[i].innerHTML = "<h4></h4><img src='/img/NODISP.JPG'" + classImage + buttonItem;
-    }
+        productSelection[i].innerHTML = titleItem + descriptionItem + priceItem + "<img src='/img/NODISP.JPG'" + classImage + buttonItem;
+        titleSelection[i].innerText = productIrishWhisky[i].name;
+        descriptionSelect[i].innerText = "Tamaño:" + productIrishWhisky[i].size;
+        priceItemSelect[i].innerText = "Precio: " + productIrishWhisky[i].price;
+    }  
 }
 
 scotchWhisky.addEventListener("click", scotishSelection);
+blendedWhisky.addEventListener("click", blendedSelection);
 bourbonWhisky.addEventListener("click", buorbonSelection);
 irishWhisky.addEventListener("click", irishSelection);
-ryeWhisky.addEventListener("click", ryeSelection);
--- scripts/products.js
+++ scripts/products.js
@@ -1,62 +1,93 @@
-export const productScotishWhisky = [
+export const productSingleMaltWhisky = [
     {
-        name: "Jameson Irish Whiskey",
+        name: "The Glenlivet 12 años",
         size: "750cc",
         productCode: "A001",
-        type: "Irish",
-        price: "1500",
+        type: "Single Malt",
+        price: "$ 9450",
     },
     {
-        name: "Jameson Black Barrel",
+        name: "Scapa Skiren",
         size: "750cc",
         productCode: "A002",
-        type: "Irish",
-        price: "1700",
+        type: "Single Malt",
+        price: "$ 11100",
     },
     {
-        name: "Jameson Caskmakes Stout Edition",
+        name: "Glenfiddich 12 años",
         size: "750cc",
         productCode: "A003",
-        type: "Irish",
-        price: "1900",
+        type: "Single Malt",
+        price: "$ 15200",
     },
     {
-        name: "Jameson Caskmakes IPA Edition",
+        name: "Clynelish 14 Años",
         size: "750cc",
         productCode: "A004",
-        type: "Irish",
-        price: "1900",
+        type: "Single Malt",
+        price: "$ 15900",
+    }
+]
+
+export const productBlendedWhisky = [
+    {
+        name: "Johnnie Walker Black Label",
+        size: "750cc",
+        productCode: "B001",
+        type: "Blended",
+        price: "$ 7700",
+    },
+    {
+        name: "Chivas Regal 12 años",
+        size: "750cc",
+        productCode: "B002",
+        type: "Blended",
+        price: "$ 6900",
+    },
+    {
+        name: "Grand Old Parr 12 años",
+        size: "750cc",
+        productCode: "B003",
+        type: "Blended",
+        price: "$ 5600",
+    },
+    {
+        name: "Grant's Triple Wood",
+        size: "750cc",
+        productCode: "B004",
+        type: "Blended",
+        price: "$ 4800",
     }
 ]
 
 export const productBourbonWhisky = [
     {
-        name: "Jameson Irish Whiskey",
+        name: "Jack Daniels Old Nº 7",
         size: "750cc",
-        productCode: "B001",
-        type: "Irish",
-        price: "1500",
+        productCode: "C001",
+        type: "Bourbon",
+        price: "$ 8300",
     },
     {
-        name: "Jameson Black Barrel",
+        name: "Jim Beam White",
         size: "750cc",
-        productCode: "B002",
-        type: "Irish",
-        price: "1700",
+        productCode: "C002",
+        type: "Bourbon",
+        price: "$ 4400",
     },
     {
-        name: "Jameson Caskmakes Stout Edition",
+        name: "Evan Williams Black",
         size: "750cc",
-        productCode: "B003",
-        type: "Irish",
-        price: "1900",
+        productCode: "C003",
+        type: "Bourbon",
+        price: "$ 7600",
     },
     {
-        name: "Jameson Caskmakes IPA Edition",
+        name: "Wild Turkey Bourbon 101",
         size: "750cc",
-        productCode: "B004",
-        type: "Irish",
-        price: "1900",
+        productCode: "C004",
+        type: "Bourbon",
+        price: "$ 7100",
     }
 ]
 
@@ -64,60 +95,29 @@ export const productIrishWhisky = [
     {
         name: "Jameson Irish Whiskey",
         size: "750cc",
-        productCode: "C001",
+        productCode: "D001",
         type: "Irish",
         price: "$ 4900",
     },
     {
         name: "Jameson Black Barrel",
         size: "750cc",
-        productCode: "C002",
+        productCode: "D002",
         type: "Irish",
         price: "$ 6700",
     },
     {
         name: "Jameson Caskmakes Stout Edition",
         size: "750cc",
-        productCode: "C003",
+        productCode: "D003",
         type: "Irish",
         price: "$ 5200",
     },
     {
         name: "Jameson Caskmakes IPA Edition",
         size: "750cc",
-        productCode: "C004",
-        type: "Irish",
-        price: "$ 5100",
-    }
-]
-
-export const productRyeWhisky = [
-    {
-        name: "Jameson Irish Whiskey",
-        size: "750cc",
-        productCode: "D001",
-        type: "Irish",
-        price: "1500",
-    },
-    {
-        name: "Jameson Black Barrel",
-        size: "750cc",
-        productCode: "D002",
-        type: "Irish",
-        price: "1700",
-    },
-    {
-        name: "Jameson Caskmakes Stout Edition",
-        size: "750cc",
-        productCode: "D003",
-        type: "Irish",
-        price: "1900",
-    },
-    {
-        name: "Jameson Caskmakes IPA Edition",
-        size: "750cc",
         productCode: "D004",
         type: "Irish",
-        price: "1900",
+        price: "$ 5100",
     }
 ]
